ff33d7d236b5970bc54d23c71b2d7a4b00d6b268
ghans07 <pghans07ds@gmail.com>
2020-08-22 21:16:50 +0530

    Email validated

--- validation.js
+++ validation.js
@@ -1,38 +1,80 @@
 const mail=document.getElementById('mail');
 const password=document.getElementById('pwd');
 const Vpassword=document.getElementById('Vpwd');
-// const phone=document.getElementsByName('number');
 const phone=document.getElementById('number');
 const name=document.getElementById('uname');
-const form=document.querySelector('form');
-// const btn=document.querySelector('button');
 const btn=document.getElementById('submit');
+const form=document.querySelector('form');
 
+form.addEventListener('submit',(event) => {
+    event.preventDefault();
+    validate();
+})
 
-
-console.log(phone,mail,form);
-
-btn.onclick=function(e){
-    if(phone.value === '')
-    {   
-        e.preventDefault();
-        const sent=document.getElementById('Mobile').querySelector('small');
-        let ele=document.getElementById('Mobile').querySelectorAll('i')[1];
-        console.log(ele);
-        ele.style.visibility='visible';
-        sent.style.visibility='visible';
-        sent.innerText="Number is invalid";
-        phone.style.borderColor='rgb(255,0,0)';
+//validate mail through Regular Expression...
+const isMail = (mailVal) =>{
+    let regex=/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+    if(regex.test(mailVal)){
+        return true;
     }
-    else
-    {
-        e.preventDefault();
-        document.getElementById('Mobile').querySelector('small').innerText="";
-        phone.style.borderColor='#2ecc71';
-        let ele=document.getElementById('Mobile').querySelectorAll('i')[0];
-        console.log(ele);
-        ele.style.visibility='visible';
+    return false;
+}
+
+//validation function...
+const validate = () =>{
+     
+    const mailVal= mail.value.trim();
+    const passwordVal= password.value.trim();
+    const VpasswordVal= Vpassword.value.trim();
+    const phoneVal= phone.value.trim();
+    const nameVal=  name.value.trim();
+
+    //Email validation...
+    if(mailVal===""){
+        setErrormsg(mail,"mail can't be empty!");
+    }else if(!isMail(mailVal)){
+        setErrormsg(mail,"Invalid Mail");
+    }else{
+        setSuccessmsg(mail);
     }
 }
+
+function setErrormsg(input,errormsgs){
+    const field=input.parentElement;
+    const small=field.querySelector('small');
+    field.className="field error";
+    small.innerText=errormsgs;
+    // console.log(field,small);
+}
+
+function setSuccessmsg(input){
+    const field=input.parentElement;
+    field.className="field success";
+}
+
+
+// btn.onclick=function(e){
+//     if(phone.value === '')
+//     {   
+//         e.preventDefault();
+//         const sent=document.getElementById('Mobile').querySelector('small');
+//         let ele=document.getElementById('Mobile').querySelectorAll('i')[1];
+//         console.log(ele);
+//         ele.style.visibility='visible';
+//         sent.style.visibility='visible';
+//         sent.innerText="Number is invalid";
+//         phone.style.borderColor='rgb(255,0,0)';
+//     }
+//     else
+//     {
+//         e.preventDefault();
+//         document.getElementById('Mobile').querySelector('small').innerText="";
+//         phone.style.borderColor='#2ecc71';
+//         let ele=document.getElementById('Mobile').querySelectorAll('i')[0];
+//         console.log(ele);
+//         ele.style.visibility='visible';
+//     }
+// }
+
 
 
